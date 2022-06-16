615cb5604a51f67f9aac15c778fe3ce6f737097d
Kevin Wagner <kevin_wagner4493@web.de>
2022-06-16 15:42:16 +0200

    added some mock datas

---
+++ src/app/mock-bookings.ts
@@ -0,0 +1,33 @@
+import { Booking } from "./booking";
+
+export const Bookings: Booking[] = [
+    {
+        id: 1,
+        name: 'Kevin Wagner',
+        roomNumber: 100,
+        startDate: new Date(),
+        endDate: new Date("2021-07-21")
+    },
+    {
+        id: 2,
+        name: 'Max Mustermann',
+        roomNumber: 50,
+        startDate: new Date("2021-07-25"),
+        endDate: new Date("2021-08-01")
+    },
+    {
+        id: 1,
+        name: 'John Doe',
+        roomNumber: 2,
+        startDate: new Date("2021-09-05"),
+        endDate: new Date("2021-09-19")
+    },
+    {
+        id: 1,
+        name: 'Maria Doemann',
+        roomNumber: 105,
+        startDate: new Date("2021-08-04"),
+        endDate: new Date("2021-08-11")
+    }
+    
+];
